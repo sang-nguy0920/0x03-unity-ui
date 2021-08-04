e2c774e84ad7d3c7f7e2de1557e48ef137152aae
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-04 11:56:31 -0500

    updated for task 1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,16 @@ public class PlayerController : MonoBehaviour
         if ( Input.GetKey("a") )
         {
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0 , 0);
+        }
 
+        if ( Input.GetKey("w") )
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if ( Input.GetKey("s") )
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
         }
     }
 }
