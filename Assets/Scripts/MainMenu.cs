72129112892c50b578ebbe39446ed0597d2bc6a6
sangnguyen-tul-0920 <70987304+sangnguyen-tul-0920@users.noreply.github.com>
2021-08-11 10:27:45 -0500

    added code for task 12

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,6 +12,17 @@ public class MainMenu : MonoBehaviour
 
     public void PlayMaze()
     {
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
 
         SceneManager.LoadScene("maze");
         
